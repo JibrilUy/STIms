99f09e19677a3eec946fda7dce83e5e98acd8cba
CampioneJibril <nottherealgabbyacc@gmail.com>
2023-02-18 23:29:36 +0800

    Image works but i want it to be saved on the device

--- app/src/main/java/com/example/stims_v9/Button/EditProfile.java
+++ app/src/main/java/com/example/stims_v9/Button/EditProfile.java
@@ -1,16 +1,25 @@
 package com.example.stims_v9.Button;
 
+import static android.app.Activity.RESULT_OK;
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.stims_v9.Login.Register;
@@ -20,9 +29,16 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 
 public class EditProfile extends Fragment {
 
+    private static final int PICK_IMAGE_REQUEST_CODE = 11;
 
     FirebaseUser currentUser;
     FirebaseAuth mAuth;
@@ -30,6 +46,8 @@ public class EditProfile extends Fragment {
     EditText editTextProfileName, editTextProfileStudentNumber, editTextProfileSection, editTextProfileParentEmail, editTextProfileQuote;
 
     Button btnAddUserData, btnProfileEditExit;
+
+    ImageView imageViewEditProfilePic;
 
     String userEmail, userName, userStudentNumber, userSection, userParentEmail, userQuote;
 
@@ -58,9 +76,17 @@ public class EditProfile extends Fragment {
         editTextProfileParentEmail = v.findViewById(R.id.editTextProfileParentEmail);
         editTextProfileQuote = v.findViewById(R.id.editTextProfileQuote);
 
+        imageViewEditProfilePic = v.findViewById(R.id.imageViewEditProfilePic);
+
         btnAddUserData = v.findViewById(R.id.btnAddUserData);
         btnProfileEditExit = v.findViewById(R.id.btnProfileEditExit);
 
+        imageViewEditProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImage();
+            }
+        });
 
         btnAddUserData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,7 +101,6 @@ public class EditProfile extends Fragment {
                 openProfileActivity();
             }
         });
-
 
 
 
@@ -129,7 +154,84 @@ public class EditProfile extends Fragment {
         updateUserData(userParentEmail, userDatabaseRef,"parent_email");
         updateUserData(userQuote, userDatabaseRef,"quote");
 
+    }
 
+
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PICK_IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            // Save the image URI in shared preferences
+            saveImageUriToSharedPreferences(imageUri);
+            // Set the image in the ImageView
+            imageViewEditProfilePic.setImageURI(imageUri);
+        }
+    }
+
+    private void saveImageUriToSharedPreferences(Uri imageUri) {
+        if (imageUri == null) {
+            Log.d("SAVE_IMAGE_URI", "Image URI is null");
+            return;
+        }
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("my_preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("image_uri", imageUri.toString());
+        boolean saved = editor.commit();
+        if (saved) {
+            Log.d("SAVE_IMAGE_URI", "Image URI saved: " + imageUri);
+        } else {
+            Log.d("SAVE_IMAGE_URI", "Failed to save image URI");
+        }
+    }
+
+
+
+    private Uri getImageUriFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("my_preferences", Context.MODE_PRIVATE);
+        String imageUriString = sharedPreferences.getString("image_uri", "");
+        if (!imageUriString.equals("")) {
+            return Uri.parse(imageUriString);
+        } else {
+            return null;
+        }
+    }
+
+    private void saveImageToInternalStorage(Uri imageUri) {
+        String imageName = "STIms_profile_image.jpg";
+        try {
+            // Open a stream to read the image file
+            InputStream inputStream = getActivity().getContentResolver().openInputStream(imageUri);
+            // Create a new file in internal storage
+            File internalFile = new File(getActivity().getFilesDir(), imageName);
+            // Open a stream to write the image file to internal storage
+            OutputStream outputStream = new FileOutputStream(internalFile);
+            // Copy the image to internal storage
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+            // Close the streams
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
+            // Save the path of the new file to a shared preference or a database
+            SharedPreferences preferences = getActivity().getSharedPreferences("MyPrefs", MODE_PRIVATE);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString("image_path", internalFile.getAbsolutePath());
+            editor.apply();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- app/src/main/java/com/example/stims_v9/Button/ProfileActivity.java
+++ app/src/main/java/com/example/stims_v9/Button/ProfileActivity.java
@@ -2,12 +2,22 @@ package com.example.stims_v9.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -24,14 +34,26 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
 public class ProfileActivity extends AppCompatActivity {
 
-    Button btnSignOut, btnExit, btnEditProfile;
+    private static final int PERMISSION_REQUEST_CODE = 10;
 
+
+    Button btnSignOut, btnExit, btnEditProfile, btnTrial;
+
+    TextView textViewProfileBlank1,textViewProfileBlank2,textViewProfileBlank3,textViewProfileBlank4,textViewProfileBlank5,textViewProfileBlank6;
     TextView textViewProfileEmail, textViewProfileName, textViewProfileStudentNumber, textViewProfileSection, textViewProfileParentEmail, textViewProfileQuote;
     FirebaseUser currentUser;
     FirebaseAuth mAuth;
 
+    Uri savedImageUri;
     ImageView imageViewProfilePic;
 
     String userEmail;
@@ -44,33 +66,72 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is granted
+
+
+
+
+        } else {
+            // Request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PERMISSION_REQUEST_CODE);
+        }
+
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
 
-        btnSignOut = findViewById(R.id.btnSignOut);
-        btnExit = findViewById(R.id.btnExit);
-        btnEditProfile = findViewById(R.id.btnEditProfile);
-
-        textViewProfileEmail = findViewById(R.id.textViewProfileEmail);
-        textViewProfileName = findViewById(R.id.textViewProfileName);
-        textViewProfileStudentNumber = findViewById(R.id.textViewProfileStudentNumber);
-        textViewProfileSection = findViewById(R.id.textViewProfileSection);
-        textViewProfileParentEmail = findViewById(R.id.textViewProfileParentEmail);
-        textViewProfileQuote = findViewById(R.id.textViewProfileQuote);
-
-        imageViewProfilePic = findViewById(R.id.imageViewProfilePic);
+        initializeVariables();
 
         displayUserData();
 
+        if (savedImageUri != null) {
+        }
+
+
+        btnTrial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savedImageUri = getImageUriFromSharedPreferences();
+                imageViewProfilePic.setImageURI(savedImageUri);
+
+            }
+        });
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        imageViewProfilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
 
         btnEditProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                hideButton(btnSignOut, btnExit, btnEditProfile);
-                hideText(textViewProfileEmail);
+                hideButton(btnSignOut, btnExit, btnEditProfile, btnTrial);
+                hideText(textViewProfileEmail,textViewProfileName,textViewProfileSection,textViewProfileStudentNumber,textViewProfileParentEmail
+                        , textViewProfileQuote, textViewProfileBlank1,textViewProfileBlank2,textViewProfileBlank3,textViewProfileBlank4,textViewProfileBlank5,textViewProfileBlank6);
                 replaceFragment(new EditProfile());
             }
         });
@@ -111,7 +172,28 @@ public class ProfileActivity extends AppCompatActivity {
         finish();
     }
 
+    public void initializeVariables(){
+        textViewProfileEmail = findViewById(R.id.textViewProfileEmail);
+        textViewProfileName = findViewById(R.id.textViewProfileName);
+        textViewProfileStudentNumber = findViewById(R.id.textViewProfileStudentNumber);
+        textViewProfileSection = findViewById(R.id.textViewProfileSection);
+        textViewProfileParentEmail = findViewById(R.id.textViewProfileParentEmail);
+        textViewProfileQuote = findViewById(R.id.textViewProfileQuote);
 
+        textViewProfileBlank1 = findViewById(R.id.textViewProfileBlank1);
+        textViewProfileBlank2 = findViewById(R.id.textViewProfileBlank2);
+        textViewProfileBlank3= findViewById(R.id.textViewProfileBlank3);
+        textViewProfileBlank4 = findViewById(R.id.textViewProfileBlank4);
+        textViewProfileBlank5 = findViewById(R.id.textViewProfileBlank5);
+        textViewProfileBlank6 = findViewById(R.id.textViewProfileBlank6);
+
+        btnSignOut = findViewById(R.id.btnSignOut);
+        btnExit = findViewById(R.id.btnExit);
+        btnEditProfile = findViewById(R.id.btnEditProfile);
+        btnTrial = findViewById(R.id.btnTrial);
+
+        imageViewProfilePic = findViewById(R.id.imageViewProfilePic);
+    }
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -119,14 +201,28 @@ public class ProfileActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
-    public void hideButton(Button button,Button button2,Button button3) {
+    public void hideButton(Button button,Button button2,Button button3,Button button4) {
         button.setVisibility(View.GONE);
         button2.setVisibility(View.GONE);
         button3.setVisibility(View.GONE);
+        button4.setVisibility(View.GONE);
+        imageViewProfilePic.setVisibility(View.GONE);
     }
 
-    public void hideText(TextView textView){
-        textView.setVisibility(View.GONE);
+    public void hideText(TextView textView1, TextView textView2, TextView textView3, TextView textView4, TextView textView5, TextView textView6
+        ,TextView textview7, TextView textview8, TextView textview9, TextView textview10, TextView textview11, TextView textview12){
+        textView1.setVisibility(View.GONE);
+        textView2.setVisibility(View.GONE);
+        textView3.setVisibility(View.GONE);
+        textView4.setVisibility(View.GONE);
+        textView5.setVisibility(View.GONE);
+        textView6.setVisibility(View.GONE);
+        textview7.setVisibility(View.GONE);
+        textview8.setVisibility(View.GONE);
+        textview9.setVisibility(View.GONE);
+        textview10.setVisibility(View.GONE);
+        textview11.setVisibility(View.GONE);
+        textview12.setVisibility(View.GONE);
     }
 
     public void displayTextOnTextView(TextView textView, String string){
@@ -134,12 +230,14 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     public void displayUserData(){
-        getValueFromUserData("email", textViewProfileEmail);
-        getValueFromUserData("name", textViewProfileName);
-        getValueFromUserData("student_number", textViewProfileStudentNumber);
-        getValueFromUserData("section", textViewProfileSection);
-        getValueFromUserData("parent_email", textViewProfileParentEmail);
-        getValueFromUserData("quote", textViewProfileQuote);
+        if(currentUser != null) {
+            getValueFromUserData("email", textViewProfileEmail);
+            getValueFromUserData("name", textViewProfileName);
+            getValueFromUserData("student_number", textViewProfileStudentNumber);
+            getValueFromUserData("section", textViewProfileSection);
+            getValueFromUserData("parent_email", textViewProfileParentEmail);
+            getValueFromUserData("quote", textViewProfileQuote);
+        }
     }
     public void getValueFromUserData(String childNode, TextView textView){
 
@@ -160,10 +258,27 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, access media files
 
-
-
-
+            }
+        }
+    }
+    private Uri getImageUriFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("my_preferences", Context.MODE_PRIVATE);
+        String imageUriString = sharedPreferences.getString("image_uri", "");
+        if (!imageUriString.equals("")) {
+            Log.d("SAVE_IMAGE_URI", "Retrieved image URI from shared preferences: " + imageUriString);
+            return Uri.parse(imageUriString);
+        } else {
+            return null;
+        }
+    }
 
 
 
